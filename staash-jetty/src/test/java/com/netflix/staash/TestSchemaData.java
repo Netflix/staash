7ba03ece8e7e54af1670b986d505426fb914e606
shyamalan <shyam.singh@outlook.com>
2014-01-09 13:03:28 -0800

    removing tomcat dependencies missing from the artifactory

--- staash-jetty/src/test/java/com/netflix/staash/TestSchemaData.java
+++ staash-jetty/src/test/java/com/netflix/staash/TestSchemaData.java
@@ -25,7 +25,6 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import com.google.common.base.Supplier;
 import com.google.common.collect.Lists;
 import com.google.inject.AbstractModule;
