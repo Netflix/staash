bcdfcf26bb6a097acd941bb903a6060a7ba976ce
Shyam Singh <ssingh@netflix.com>
2014-02-03 16:55:04 -0800

    uploading new gitignore adding the missing file

---
+++ staash-web/src/main/java/com/netflix/staash/web/GuiceServletConfig.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * /*
+ *  *
+ *  *  Copyright 2013 Netflix, Inc.
+ *  *
+ *  *     Licensed under the Apache License, Version 2.0 (the "License");
+ *  *     you may not use this file except in compliance with the License.
+ *  *     You may obtain a copy of the License at
+ *  *
+ *  *         http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  *     Unless required by applicable law or agreed to in writing, software
+ *  *     distributed under the License is distributed on an "AS IS" BASIS,
+ *  *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *     See the License for the specific language governing permissions and
+ *  *     limitations under the License.
+ *  *
+ *  *
+ ******************************************************************************/
+package com.netflix.staash.web;
+
+import com.google.inject.Injector;
+import com.google.inject.servlet.GuiceServletContextListener;
+import com.netflix.governator.guice.LifecycleInjector;
+import com.netflix.staash.cassandra.discovery.EurekaModule;
+import com.netflix.staash.rest.modules.PaasPropertiesModule;
+import com.netflix.staash.rest.resources.PaasAdminResourceImplNew;
+import com.netflix.staash.rest.resources.PaasDataResourceImplNew;
+import com.sun.jersey.guice.JerseyServletModule;
+import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
+
+public class GuiceServletConfig extends GuiceServletContextListener {
+    @Override
+    protected Injector getInjector() {
+        return LifecycleInjector.builder()
+                .withModules(
+                    new EurekaModule(),
+                     new PaasPropertiesModule(),   
+                    new JerseyServletModule() {
+                        @Override
+                        protected void configureServlets() {
+                            bind(GuiceContainer.class).asEagerSingleton();
+                            bind(PaasAdminResourceImplNew.class);
+                            bind(PaasDataResourceImplNew.class);
+                            serve("/*").with(GuiceContainer.class);
+                        }
+                    }
+                )
+                .createInjector();
+    }
+}
